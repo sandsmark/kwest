18f9e57efea72d5f05557ce3cf4a98d810b144d5
pbienst <pbienst@72236c6e-d75a-4a7e-85b7-6709e563ff4a>
2007-09-06 16:23:43 +0000

    Initial import

---
+++ kwest-web/upload_web.sh
@@ -0,0 +1,4 @@
+#scp -r * mnemosyne-proj.sf.net:/home/groups/k/kw/kwest/htdocs/
+
+scp *.html mnemosyne-proj.sf.net:/home/groups/k/kw/kwest/htdocs/
+#scp *.css  mnemosyne-proj.sf.net:/home/groups/m/mn/mnemosyne-proj/htdocs/
